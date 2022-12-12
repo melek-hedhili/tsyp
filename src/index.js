2bd3e555833134cf581687fd49437b0e26e48de4
melek <melekhedhili7@gmail.com>
2022-12-12 20:28:13 +0100

    updated

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ import { ItemProvider } from "./hooks/useItem";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <AuthProvider>
         <ItemProvider>
           <App />
